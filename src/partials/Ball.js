2a20818a54fc23794286f154f6132a65b48730cd
Albert Owusu <admin@admins-MacBook-Pro.local>
2020-01-29 11:09:15 -0500

    first commit

---
+++ src/partials/Ball.js
@@ -0,0 +1,28 @@
+import { SVG_NS } from "../settings";
+
+export default class Ball {
+  constructor(radius, boardWidth, boardHeight, fill) {
+    this.radius = radius;
+    this.boardWidth = boardWidth;
+    this.boardHeight = boardHeight;
+    this.fill = fill;
+    this.direction = 1;
+    //resets the ball in the middle of the board
+    this.reset();
+  }
+  reset() {
+    //code to center ball and for movement
+  }
+
+  render(svg) {
+    let ball = document.createElementNS(SVG_NS, "circle");
+    //yourcode here
+    ball.setAttributeNS(null, "r", this.radius);
+    ball.setAttributeNS(null, "cx", this.boardWidth / 2);
+    ball.setAttributeNS(null, "cy", this.boardHeight / 2);
+    ball.setAttributeNS(null, "fill", this.fill);
+
+    //paint the screen
+    svg.appendChild(ball);
+  }
+}
